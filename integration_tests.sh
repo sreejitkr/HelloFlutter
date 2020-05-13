0ca981e79ed8c65d8ead0386122d1bbc3afae1cc
Varuna Srivastava <srivastavavaruna@gmail.com>
2020-05-13 16:33:17 +0530

    Added a integration tests for sample app

---
+++ integration_tests.sh
@@ -0,0 +1,2 @@
+flutter drive --target=test_driver/e2e.dart --driver=test_driver/e2e_test.dart
+flutter drive --target=test_driver/e2e.dart --driver=test_driver/e2e_test2.dart
--- test_driver/e2e_test2.dart
+++ test_driver/e2e_test2.dart
@@ -5,19 +5,19 @@ import 'package:screenshots/screenshots.dart';
 import 'package:image/image.dart';
 import 'package:diff_image/diff_image.dart';
 import 'package:ozzie/ozzie.dart';
-
+@Tags(const ['varuna'])
 void main() {
 
-  group('Smoke Tests', () {
+  group('hi', () {
     final enterItemNameLabel = find.byValueKey('labeltext');
     final enterItemName = find.byValueKey('textfield');
     FlutterDriver driver;
     Ozzie ozzie;
 
     setUpAll(() async {
-      new Directory('/tmp/screenshots11').create();
+      new Directory('/tmp/screenshots').create();
       driver = await FlutterDriver.connect();
-      ozzie = Ozzie.initWith(driver, groupName: 'E2E1');
+      ozzie = Ozzie.initWith(driver, groupName: 'SmokeTest2');
     });
 
     takeScreenshot(FlutterDriver driver, String path) async {
@@ -34,30 +34,10 @@ void main() {
       }
     });
 
-    test('Test Scenario 2',() async{
-      await ozzie.takeScreenshot('Scenario2');
-    });
-    test('Test Scenario 1', () async {
-      final config = Config();
+    test('Test Scenario E2E2', () async {
       expect(await driver.getText(enterItemNameLabel), "Enter Item Name");
       expect(await driver.getText(enterItemName), "Add Item");
-      await screenshot(driver, config, 'varuna');
-      await ozzie.takeScreenshot('Scenario1');
-//      await screenshots(
-//          configPath: 'screenshots.yaml',
-//          mode: 'recording',
-//          flavor: 'no flavor',
-//          isBuild: false,
-//          isVerbose: true);
-//      await screenshots(
-//          configPath: 'screenshots.yaml',
-//          mode: 'comparison',
-//          flavor: 'no flavor',
-//          isBuild: false,
-//          isVerbose: true);
-//      await takeScreenshot(driver, 'screenshots/anc.png');
-//      var diff = await DiffImage.compareFromUrl('file://screenshots/anc.png', 'file://screenshots/varuna.png');
-//      print('The difference between images is: $diff %');
+      await ozzie.takeScreenshot('E2E2');
     });
   });
 }
